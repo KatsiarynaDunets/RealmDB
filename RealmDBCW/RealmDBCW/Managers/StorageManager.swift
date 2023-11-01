2bd8a90f238e4046325f11a4b0a0cf3e378beb08
KatsiarynaDunets <kate.dunets@gmail.com>
2023-11-01 18:57:22 +0100

    Version 1.0

---
+++ RealmDBCW/RealmDBCW/Managers/StorageManager.swift
@@ -0,0 +1,39 @@
+//
+//  StorageManager.swift
+//  RealmDBCW
+//
+//  Created by Kate on 01/11/2023.
+//
+
+import Foundation
+import RealmSwift
+
+let realm = try! Realm()
+
+class StorageManager {
+    
+    static func getAllTasksLists() -> Results<TasksList> {
+        realm.objects(TasksList.self)//.sorted(byKeyPath: "name")
+    }
+    
+    static func deleteAll() {
+        do {
+            try realm.write {
+                realm.deleteAll()
+            }
+        } catch {
+            print("deleteAll error: \(error)")
+        }
+    }
+    
+    static func saveTasksList(tasksList: TasksList) {
+        do {
+            try realm.write {
+                realm.add(tasksList)
+            }
+        } catch {
+            print("saveTasksList error: \(error)")
+        }
+    }
+}
+
---
+++ RealmDBCW/RealmDBCW/Models/ListModel.swift
@@ -0,0 +1,16 @@
+//
+//  ListModel.swift
+//  RealmDBCW
+//
+//  Created by Kate on 01/11/2023.
+//
+
+import Foundation
+import RealmSwift
+
+class Task: Object {
+    @Persisted var name = ""
+    @Persisted var note = ""
+    @Persisted var date = Date()
+    @Persisted var isComplete = false
+}
